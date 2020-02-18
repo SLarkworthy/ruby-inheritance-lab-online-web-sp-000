384c66b6b6ad22668e92c87190248b9fdf9c390a
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-18 15:26:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,14 @@
 require_relative "./user.rb"
 
 class Student < User
+  attr_accessor :knowledge
+  
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(string)
+    @knowledge << string
+  end
 
 end
